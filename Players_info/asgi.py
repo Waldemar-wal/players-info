5b1142799b9111b0b729ede376944f0b50c2fcb3
Vova Savenkov <sava.v80@gmail.com>
2025-03-21 14:17:38 +0200

    add models and migrate

--- Players_info/asgi.py
+++ Players_info/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Players_info.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Players_info.settings")
 
 application = get_asgi_application()
--- players/models.py
+++ players/models.py
@@ -1,3 +1,50 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class EquipmentType(models.Model):
+    type = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.type
+
+
+class Player(AbstractUser):
+    level = models.IntegerField(default=1)
+    power = models.IntegerField(default=0)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "player"
+        verbose_name_plural = "players"
+
+    def __str__(self):
+        return (f"Player: {self.username}, race:{self.race.name}, "
+                f"level: {self.level}, power: {self.power}")
+
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    type = models.ForeignKey(EquipmentType, on_delete=models.CASCADE)
+    player = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="equipments",
+        blank=True
+    )
+
+    def __str__(self):
+        return f"{self.name}({self.type.type}): {self.description}"
+
+
+
+
